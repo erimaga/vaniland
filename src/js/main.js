64202b9811c152fed45566b57c283101c5fb2afd
Eric Maina Gathoni <ericbro29@gmail.com>
2020-12-01 16:24:03 +0300

    Add routes

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,26 @@
 import '../styles/main.scss';
+import routes from '../routes';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Generate HTML elements for the routes
+  routes.forEach((route) => {
+    const gameMenu = document.getElementById('game-menu');
+    const pageMenu = document.getElementById('page-menu');
+    const uiMenu = document.getElementById('ui-menu');
+
+    const link = document.createElement('a');
+    link.classList.add('link');
+    link.href = route.route;
+    link.textContent = route.name;
+
+    if (route.page) {
+      return pageMenu.appendChild(link);
+    }
+    if (route.game) {
+      return gameMenu.appendChild(link);
+    }
+    if (route.ui) {
+      return uiMenu.appendChild(link);
+    }
+  });
+});
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    name: 'Home',
+    route: '/',
+    page: true,
+  },
+  {
+    name: 'About',
+    route: '/about',
+    page: true,
+  },
+  {
+    name: 'Rock Paper Scissors',
+    route: '/games/rock-paper-scissors',
+    game: true,
+  },
+];
